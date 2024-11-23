f5cafc288fa66cb8155882579fbeb24a1439644e
Elliott <ellbutt@gmail.com>
2024-11-23 19:13:22 -0330

    added additional get methods (id, firstname, lastname, fullname, email, phone, membershipduration)

--- src/main/java/com/keyin/member/MemberController.java
+++ src/main/java/com/keyin/member/MemberController.java
@@ -3,6 +3,8 @@ package com.keyin.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @CrossOrigin
 public class MemberController {
@@ -12,6 +14,41 @@ public class MemberController {
     @GetMapping("/member/all")
     public Iterable<Member> getAllMember() {
         return memberService.getAllMembers();
+    }
+
+    @GetMapping("/member/id/{id}")
+    public Optional<Member> getMemberById(@PathVariable int id) {
+        return memberService.getMemberById(id);
+    }
+
+    @GetMapping("/member/last_name/{lastName}")
+    public Iterable<Member> getMembersByLastName(@PathVariable String lastName) {
+        return memberService.getMembersByLastName(lastName);
+    }
+
+    @GetMapping("/member/first_name/{firstName}")
+    public Iterable<Member> getMembersByFirstName(@PathVariable String firstName) {
+        return memberService.getMembersByFirstName(firstName);
+    }
+
+    @GetMapping("/member/full_name/{firstName}/{lastName}")
+    public Optional<Member> getMemberByFullName(@PathVariable String firstName, @PathVariable String lastName) {
+        return memberService.getMemberByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @GetMapping("/member/email/{email}")
+    public Optional<Member> getMemberByEmail(@PathVariable String email) {
+        return memberService.getMemberByEmail(email);
+    }
+
+    @GetMapping("/member/phone_number/{phoneNumber}")
+    public Optional<Member> getMemberByPhoneNumber(@PathVariable String phoneNumber) {
+        return memberService.getMemberByPhoneNumber(phoneNumber);
+    }
+
+    @GetMapping("/member/membership_duration/{duration}")
+    public Iterable<Member> getMembersByMembershipDuration(@PathVariable int duration) {
+        return memberService.getMembersByMembershipDuration(duration);
     }
 
     @PostMapping("/member")
--- src/main/java/com/keyin/member/MemberService.java
+++ src/main/java/com/keyin/member/MemberService.java
@@ -3,6 +3,8 @@ package com.keyin.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MemberService {
     @Autowired
@@ -10,6 +12,34 @@ public class MemberService {
 
     public Iterable<Member> getAllMembers() {
         return memberRepository.findAll();
+    }
+
+    public Optional<Member> getMemberById(int userId) {
+        return memberRepository.findById(userId);
+    }
+
+    public Iterable<Member> getMembersByFirstName(String lastName) {
+        return memberRepository.getMembersByFirstName(lastName);
+    }
+
+    public Iterable<Member> getMembersByLastName(String lastName) {
+        return memberRepository.getMembersByLastName(lastName);
+    }
+
+    public Optional<Member> getMemberByFirstNameAndLastName(String firstName, String lastName) {
+        return memberRepository.getMemberByFirstNameAndLastName(firstName, lastName);
+    }
+
+    public Optional<Member> getMemberByEmail(String email) {
+        return memberRepository.getMemberByEmail(email);
+    }
+
+    public Optional<Member> getMemberByPhoneNumber(String phoneNumber) {
+        return memberRepository.getMemberByPhoneNumber(phoneNumber);
+    }
+
+    public Iterable<Member> getMembersByMembershipDuration(int duration) {
+        return memberRepository.getMemberByMembershipDurationInMonths(duration);
     }
 
     public Member addMember(Member member) {
